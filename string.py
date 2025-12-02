31161465dc9e8ff8fb241aa4a20412f25aad434d
sumitk7782 <sumitk7782@gmail.com>
2025-12-02 16:42:26 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,22 @@
+"""str1 = "apana"
+len1 = len(str1)
+print(len1)
+
+str2 = "collage"
+len2 = len(str2)
+print(len2)
+
+final_str = (str1+ ""+ str2)
+print(final_str)
+print(len(final_str))"""
+
+#INDEXING
+
+"""str = "apna college"
+ch = str[6]
+print(ch)"""
+
+#slicing index
+
+str = "apple"
+print(str[-3:-1])
